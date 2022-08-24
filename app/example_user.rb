d1b38ca4cebc570ef5fbdd1293a8c81b8954a7e0
a-maci29 <alexa.macias@prx.og>
2022-08-24 12:45:38 -0400

    begin styling

--- app/example_user.rb
+++ app/example_user.rb
@@ -1,9 +1,9 @@
 # class User
-#   attr_accessor :name, :emai
+#   attr_accessor :name, :email   #  attribute accessors
 
-# def initialize(attributes = {})
-#   @name  = attributes[:name]
-#   @email = attributes[:email]
+# def initialize(attributes = {})   # creating empty hash?
+#   @name  = attributes[:name]  #  getter and setter variables
+#   @email = attributes[:email]   #  will be instance variables when created
 # end
 
 # def formatted_email
